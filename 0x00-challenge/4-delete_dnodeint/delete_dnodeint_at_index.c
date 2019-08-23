e45b06292c93613ec0652c33a5d58f565102c23a
arleybri18 <arleybri18@gmail.com>
2019-08-23 23:41:50 +0000

    fix pointer of the list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		/*(*head)->prev->prev = (*head)->prev;*/
 		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
